aa30da144e4089cf40fd1ed8d720902808016507
Trung <dotrantrung@gmail.com>
2016-04-17 19:26:02 -0700

    finished adding

---
+++ app/src/main/java/com/example/trungdo/lab2_addition/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.trungdo.lab2_addition;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+    public void sendMessage(View view) {
+        EditText a = (EditText) findViewById(R.id.editText1);
+        EditText b = (EditText) findViewById(R.id.editText2);
+        int numA = Integer.parseInt(a.getText().toString()) ;
+
+        int numB = Integer.parseInt(b.getText().toString()) ;
+        int sum = numA + numB;
+
+        TextView sumTV = (TextView) findViewById(R.id.textView3);
+        sumTV.setText(String.valueOf(sum));
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
